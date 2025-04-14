66820ff38fe2bc596fe2e367fde1a347d7f7c8e5
Elmegaard <elmegaard39@gmail.com>
2025-04-14 20:00:45 +0200

    Added lucion and translucent party dc6

--- d2gl/src/modules/hd_text/variables.h
+++ d2gl/src/modules/hd_text/variables.h
@@ -215,9 +215,9 @@ inline std::vector<D2TextInfo> g_options_texts = {
 	{ 1, { 6, 26 }, L"SHOW ITEM LEVEL", TextAlign::Left }, // ShowItemLevel.dc6
 	{ 1, { 47, 26 }, L"CHAT NOTIFICATIONS", TextAlign::Left }, // LootNotify.dc6
 	{ 0, { 82, 26 }, L"LEVEL", TextAlign::Left }, // FilterLevel.dc6
-	{ 0, { 163, 26 }, L"???? AURA", TextAlign::Left }, // TempAura.dc6
+	{ 0, { 203, 26 }, L"LUCION AURA", TextAlign::Left }, // LucionAura.dc6
 	{ 0, { 65, 26 }, L"NEW", TextAlign::Right }, // MenuNew.dc6
-
+	{ 1, { 58, 26 }, L"TRANSLUCENT PARTY", TextAlign::Left }, // TranslucentParty.dc6
 };
 inline std::map<uint32_t, std::vector<D2TextInfo>> g_options_text_others = {
 	{ LANG_ESP, {
